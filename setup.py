25b6c7781b8fcd565cdf319d52a6a8dfc86b7262
NAGY, Attila <nagy.attila@gmail.com>
2021-03-02 14:27:05 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['Click>=7.0', 'networkx', 'more-itertools', ]
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest>=3', ]
+
+setup(
+    author="NAGY, Attila",
+    author_email='nagy.attila@gmail.com',
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Pandas DAG",
+    entry_points={
+        'console_scripts': [
+            'pandag=pandag.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='pandag',
+    name='pandag',
+    packages=find_packages(include=['pandag', 'pandag.*']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/Mikata-Project/pandag',
+    version='0.0.1',
+    zip_safe=False,
+)
