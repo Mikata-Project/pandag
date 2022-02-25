1c0d679f0e388136189560e01c98f56650b66bc5
bra-fsn <nagy.attila@gmail.com>
2022-02-25 15:13:32 +0100

    Bump ver

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Mikata-Project/pandag',
-    version='0.0.6',
+    version='0.0.7',
     zip_safe=False,
 )
