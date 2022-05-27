324458994d9391e791ad3958c01706736451e3d5
Jeff <bitcloud22@gmail.com>
2022-05-27 16:33:03 -0700

    spelling

--- pandag/pandag.py
+++ pandag/pandag.py
@@ -139,7 +139,7 @@ class Pandag:
         """
         # generate a unique column name
         node_col = f'{self.uuid}_curr_node'
-        # TODO: Add validation to make ensure ids or created top to bottom.
+        # TODO: Add validation to make ensure ids are created top to bottom.
         for src_node_id, dst_node_id in sorted(self.G.edges):
             if node_col not in df.columns:
                 # this is the first node we're visiting, add its id to the
